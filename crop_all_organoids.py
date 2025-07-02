0514043256a28018200f096447064918306e83c9
rzhou-space <107350560+rzhou-space@users.noreply.github.com>
2025-07-02 18:52:41 +0200

    Add files via upload

--- crop_all_organoids.py
+++ crop_all_organoids.py
@@ -1,8 +1,6 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[1]:
-
 
 import numpy as np
 import matplotlib.pyplot as plt
@@ -11,8 +9,6 @@ import tifffile
 
 
 # # Single time all organoids crop
-
-# In[9]:
 
 
 # # Import the original tif file at one time point.
@@ -29,9 +25,6 @@ import tifffile
 #     top_level_keys = list(f.keys())
 #     print("Detected Number of Organoids:", len(top_level_keys))
 #     bbx = f["organoid_10"]["42"]["bbox"][:]
-
-
-# In[2]:
 
 
 def find_org_at_tp(bbox_h5_path, tp):
@@ -52,9 +45,6 @@ def find_org_at_tp(bbox_h5_path, tp):
     return org_at_tp
 
 
-# In[23]:
-
-
 def collect_coordinates(bbox_h5_path, tp):
     # collect the coordinates of all bounding boxes and corresponding organoids. Buil list of tupples. 
     boxes = []
@@ -67,9 +57,6 @@ def collect_coordinates(bbox_h5_path, tp):
             x1, y1, z1, x2, y2, z2 = f[org][tp]["bbox"][:] # Coordinates of the bounding box. 
             boxes.append(((x1, y1, z1), (x2, y2, z2), org)) # Save the corners of bbox and organoids as tuple. 
     return boxes
-
-
-# In[15]:
 
 
 def bbox_mask_single_box(bbox_h5_path, tp, images):
@@ -88,16 +75,13 @@ def bbox_mask_single_box(bbox_h5_path, tp, images):
     return empty_volume
 
 
-# In[14]:
-
-
 def bbox_mask(bbox_h5_path, tp, images):
     # Create an empty volume with the same shape as the input original data. 
     empty_volume = np.zeros((np.shape(images)))
     # collect all the bounding boxes that present at the given time point tp. 
     boxes = collect_coordinates(bbox_h5_path, tp)
     # Assign values to each box in the volume. 
-    for (start, end, label) in enumerate(boxes, start=1):
+    for i, (start, end, label) in enumerate(boxes, start=1):
         x1, y1, z1 = start
         x2, y2, z2 = end
         xmin, xmax = sorted([x1, x2])
@@ -107,9 +91,6 @@ def bbox_mask(bbox_h5_path, tp, images):
     return empty_volume
 
 
-# In[11]:
-
-
 def overlap_bbox_mask(prediction_file, original_tif_file, tp):
     # The original tif images at time point tp. 
     with tifffile.TiffFile(original_tif_file) as tif:
@@ -117,79 +98,42 @@ def overlap_bbox_mask(prediction_file, original_tif_file, tp):
     # make bounding box mask from the prediction h5 file for the given time point tp. 
     mask_volume = bbox_mask(prediction_file, tp, images)
     # Overlap the original volome at time point tp with the bounding box mask. 
-    return images*mask_volume
-
-
-# In[12]:
+    return np.array(images * mask_volume)
 
 
 tif_path = "/Users/rzhoufias.uni-frankfurt.de/Documents/PhD_Franziska/Organoid/organoid_3Dtrack/data/HCC_42_91_tif_video/original_tif_data/20181113_HCC_d0-2_t42c1_ORG.tif"
 bbox_h5_path = "/Users/rzhoufias.uni-frankfurt.de/Documents/PhD_Franziska/Organoid/predictions/organoid4D_tp_42_82_z_0_870_min_preds_10.h5"
-overlap = overlap_bbox_mask(bbox_h5_path, tif_path, "42")
-plt.imshow(overlap[200])
 
-
-# In[ ]:
-
-
-# TODO: Plot in 3D and Labeling! The plotly is not working!
-
-
-# In[18]:
-
-
-tif_path = "/Users/rzhoufias.uni-frankfurt.de/Documents/PhD_Franziska/Organoid/organoid_3Dtrack/data/HCC_42_91_tif_video/original_tif_data/20181113_HCC_d0-2_t42c1_ORG.tif"
-with tifffile.TiffFile(tif_path) as tif:
-    images = tif.asarray()  # shape: (num_frames, height, width)
-bbox_h5_path = "/Users/rzhoufias.uni-frankfurt.de/Documents/PhD_Franziska/Organoid/predictions/organoid4D_tp_42_82_z_0_870_min_preds_10.h5"
-volume = bbox_mask_single_box(bbox_h5_path, "42", images)
-
-
-# In[22]:
-
-
-plt.imshow(volume[500])
-
-
-# In[ ]:
-
-
-import plotly.graph_objects as go
-# Present the organoid bounding boxes in 3D. 
-boxes = collect_coordinates(bbox_h5_path, "42")
-# Plot all labeled voxels
-fig = go.Figure()
-for i, (_, _, label) in enumerate(boxes, start=1):
-    x, y, z = np.where(volume == i) # TODO: Can make it quicker when directly use the know coordinates of the single boxes! And the individual value assigining could also be let out!
-    fig.add_trace(go.Scatter3d(
-        x=x,
-        y=y,
-        z=z,
-        mode='markers',
-        marker=dict(size=4),
-        name=label,
-        text=[label] * len(x),  # Show label on hover
-        hoverinfo='text'
-    ))
-
-fig.update_layout(
-    title="Multiple Labeled 3D Volumes",
-    scene=dict(xaxis_title='X', yaxis_title='Y', zaxis_title='Z'),
-    width=800,
-    height=800
-)
-
-fig.write_html("Bounding Boxes.html", auto_open=True)
-
-
-# In[ ]:
+box_coordinate = collect_coordinates(bbox_h5_path, "42")
 
 
 
+# TODO: When the size of the whole 3D image is too large, can also make it possible for subregions presentation. 
+# TODO: Possible to overlap with original images?
 
 
-# In[ ]:
+from vedo import Box, Text3D, show
+
+boxes = []
+labels = []
+
+for box in box_coordinate:
+    boxes.append(box[0]+box[1])
+    labels.append(box[2][-2:])
+print(boxes)
+print(labels)
 
 
+actors = []
 
+for i, (x1,y1,z1,x2,y2,z2) in enumerate(boxes):
+    box = Box(pos=[(x1+x2)/2, (y1+y2)/2, (z1+z2)/2],
+              length=abs(x2-x1), width=abs(y2-y1), height=abs(z2-z1),
+              c='lightblue', alpha=0.3)
+    actors.append(box)
+    
+    label_pos = (x2 + 0.1, y2 + 0.1, z2 + 0.1)  # offset to be outside the box
+    label = Text3D(labels[i], pos=label_pos, s=20, c='red')
+    actors.append(label)
 
+show(actors)
